565f3e88e28781d7384db2ddbb7559b101042377
Irene <irenearribasmendez@gmail.com>
2026-06-04 21:54:54 +0200

    Create t1e1peliculas.py

---
+++ ejercicios/t1e1peliculas.py
@@ -0,0 +1,19 @@
+#Título de la película
+titulo = "Howl's Moving Castle"
+#Director de la película
+director = "Hayao Miyazaki"
+#Año de lanzamiento
+año = 2004
+#Género
+género = "Animation"
+#Duración en minutos
+duracion = 119
+#¿Tiene premios?
+tiene_premios = True
+
+print("Título", titulo)
+print("Director", director)
+print("Año", año)
+print("Género", género)
+print("Duración", duracion)
+print("Tiene premios", tiene_premios)
