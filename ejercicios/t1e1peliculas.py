75982169c57c428438594cc94d0b3d29a4e09b82
Irene <irenearribasmendez@gmail.com>
2026-06-07 17:36:36 +0200

    t1e1peliculas actualizado

--- ejercicios/t1e1peliculas.py
+++ ejercicios/t1e1peliculas.py
@@ -1,19 +1,42 @@
-#Título de la película
-titulo = "Howl's Moving Castle"
-#Director de la película
+#Declara variables para guardar la información de tu película favorita
+titulo = "El castillo ambulante"
+
 director = "Hayao Miyazaki"
-#Año de lanzamiento
+
 año = 2004
-#Género
-género = "Animation"
-#Duración en minutos
+
+género = "Animación"
+
 duracion = 119
-#¿Tiene premios?
+
 tiene_premios = True
 
-print("Título", titulo)
-print("Director", director)
-print("Año", año)
+#Muestra la información en la consola
+print("Título:", titulo)
+print("Director:", director)
+print("Año:", año)
+print("Género:", género)
+print("Duración:", duracion)
+print("¿Tiene premios?", tiene_premios)
+print()
+
+#Cambia los valores de las variables a datos en inglés
+titulo = "Howl's Moving Castle"
+
+director = "Hayao Miyazaki"
+
+año = 2004
+
+género = "Animation"
+
+duracion = 119
+
+tiene_premios = True
+
+#Vuelve a mostrar la información en la consola
+print("Título:", titulo)
+print("Director:", director)
+print("Año:", año)
 print("Género", género)
-print("Duración", duracion)
-print("Tiene premios", tiene_premios)
+print("Duración:", duracion)
+print("¿Tiene premios?", tiene_premios)
