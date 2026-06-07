95b2001d85f03de86cbfaa590ca42a512f720533
Irene <irenearribasmendez@gmail.com>
2026-06-07 18:57:06 +0200

    t1e2canciones

---
+++ ejercicios/t1e2canciones.py
@@ -0,0 +1,62 @@
+#Pide a la usuaria que introduzca la siguiente información sobre su canción favorita:
+titulo_cancion = "¿Cuál es el título de tu canción favorita?"
+titulo_artista = "¿Cuál es el nombre del artista?"
+titulo_album = "¿Cuál es el título del álbum?"
+año_lanzamiento = "¿Cuál es el año de lanzamiento?"
+duracion_cancion = "¿Cuál es la duración de la canción en segundos?"
+videoclip = "¿Tiene videoclip?"
+
+print (titulo_cancion)
+print (titulo_artista)
+print (titulo_album)
+print (año_lanzamiento)
+print (duracion_cancion)
+print (videoclip)
+print ()
+
+#Muestra la información introducida
+cancion_fav = "Alkaline"
+artista_fav = "Sleep Token"
+album_fav = "This Place Will Become Your Tomb"
+año_fav = 2021
+duracion_fav = 200,4
+video_fav = True
+
+print (cancion_fav) 
+print (artista_fav)
+print (album_fav)
+print (año_fav)
+print (duracion_fav)
+print (video_fav)
+print ()
+
+#Solicita los mismos datos de la canción que menos le guste
+nombre_cancion = "¿Cuál es el título de la canción que menos te gusta?"
+nombre_artista = "¿Cuál es el nombre del artista que menos te gusta?"
+nombre_album = "¿Cuál es el título del álbum que menos te gusta?"
+fecha_lanzamiento = "¿Cuál es el año de lanzamiento?"
+segundos_cancion = "¿Cuál es la duración de la canción en segundos?"
+video = "¿Tiene videoclip?"
+
+print (nombre_cancion)
+print (nombre_artista)
+print (nombre_album)
+print (fecha_lanzamiento)
+print (segundos_cancion)
+print (video)
+print ()
+
+#Muestra la información introducida
+cancion_hate = "Blank Space"
+artista_hate = "Taylor Swift"
+album_hate = 1989
+año_hate = 2014
+duracion_hate =210,6
+video_hate = True
+
+print (cancion_hate)
+print (artista_hate)
+print (album_hate)
+print (año_hate)
+print (duracion_hate)
+print (video_hate)
