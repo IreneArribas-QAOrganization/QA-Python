2a0ab16bddebffc132f616525dba94a431ef79ae
Irene <irenearribasmendez@gmail.com>
2026-06-08 13:31:21 +0200

    t2e1calculadora.py

---
+++ ejercicios/t2e1calculadora.py
@@ -0,0 +1,26 @@
+#Pide a la usuaria dos números
+pregunta = "Elije un número del 1 al 10"
+pregunta2 = "Ahora elije otro más"
+
+print (pregunta)
+numero1 = int(input())
+print (pregunta2)
+numero2 = int(input())
+print ()
+
+print (numero1), print (numero2)
+print ()
+
+#Muestra el resultado de:
+    #Suma 
+print ("Suma:"), print (numero1 + numero2)
+print ()
+    #Resta
+print ("Resta:"), print (numero1 - numero2)
+print ()
+    #Multiplicación
+print ("Multiplicación:"), print (numero1 * numero2)
+print ()
+    #División
+print ("División:"), print (numero1 / numero2)
+print ()
