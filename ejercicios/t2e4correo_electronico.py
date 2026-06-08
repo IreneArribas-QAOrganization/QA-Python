e57fbbb87e0578fef3d46c0f816d2e218906689f
Irene <irenearribasmendez@gmail.com>
2026-06-08 15:11:19 +0200

    t2e4correo_electronico.py

---
+++ ejercicios/t2e4correo_electronico.py
@@ -0,0 +1,26 @@
+#Pide a la usuaria que introduzca una dirección de correo electrónico
+peticion = "Escribe tu correo elctrónico:"
+
+print (peticion)
+correo = input ()
+print ()
+
+#Muestra:
+longitud = len(correo)
+mayusculas = correo.upper()
+minusculas = correo.lower()
+
+    #La longitud del correo
+print ("Longitud del correo")
+print (longitud)
+print ()
+
+    #El correo en mayúsculas
+print ("Correo en mayúsculas")
+print (mayusculas)
+print ()
+
+    #El correo en minúsculas
+print ("Correo en minúsculas")
+print (minusculas)
+print ()
