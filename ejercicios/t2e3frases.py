ec07d46e780681665af07de78b4d1c8cc2261c44
Irene <irenearribasmendez@gmail.com>
2026-06-08 14:59:53 +0200

    t2e3frases.py

---
+++ ejercicios/t2e3frases.py
@@ -0,0 +1,25 @@
+#Pide a la usuaria que introduzca una frase
+solicitud = "Escribe una frase"
+
+print (solicitud)
+frase = input ()
+print ()
+
+#Muestra:
+    #La longitud de la frase
+longitud = len(frase)
+mayusculas = frase.upper()
+minusculas = frase.lower()
+
+print ("Longitud de la frase")
+print (longitud)
+print ()
+
+    #La frase en mayúsculas
+print ("Frase en mayúsculas")
+print (mayusculas)
+print ()
+    #La frase en minúsculas
+print ("Frase en minúsculas")
+print (minusculas)
+print ()
