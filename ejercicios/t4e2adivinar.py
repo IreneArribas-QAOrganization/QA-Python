99666199546f4503a381997975e9f11b8f9b69d2
Irene <irenearribasmendez@gmail.com>
2026-06-14 14:25:53 +0200

    Create t4e2adivinar.py

--- ejercicios/t4e2adivinar.py
+++ ejercicios/t4e2adivinar.py
@@ -2,9 +2,22 @@
 pregunta = "Elije un numero del 1 al 10"
 
 print (pregunta)
-respuesta = input(int())
+respuesta = int(input())
+print ()
 
-#Crea una función que reciba un número y devuelva un mensaje de victoria si acierta y un mensaje de derrota si falla. El número ganador es el 4
+#Crea una función que reciba un número y devuelva un mensaje de victoria si acierta y un mensaje de derrota si falla (el número ganador es el 4)
+mensaje_victoria = "¡Has acertado!"
+mensaje_derrota = "No has acertado :(. Vuelve a inetantarlo"
 
+def numero (respuesta_numero):
+    if respuesta_numero >=1 and respuesta_numero <=10:
+        if respuesta_numero == 4:
+            return mensaje_victoria
+        elif respuesta_numero != 4:
+            return mensaje_derrota
+
+mensaje = numero (respuesta)
 
 #Imprime el número elegido y el mensaje de resultado por pantalla
+print ("El número elejido fue:", respuesta)
+print ("El mensaje del resultado es:", mensaje)
